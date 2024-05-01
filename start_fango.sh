92172e56435b55dfae9c78ab343db02ed6e4758e
Luis Manuel <32717016+GearFox98@users.noreply.github.com>
2024-05-01 18:30:34 -0400

    Adding fango launcher!

---
+++ start_fango.sh
@@ -0,0 +1,7 @@
+# Uncomment the line below and place your virtual environment path
+# activation script there.
+#
+# source PATH_TO_VENV_BIN/ACTIVATE
+
+# Make sure you're leaving this in the folder you found it
+python3 ui.py
